be4de12df397e756633f96e5a5530cc6bbf60761
Alve Svarén (PC) <alve@hotmail.se>
2019-07-28 23:26:28 +0200

    Now only skips up to one day and also stops if there isn't any player currently sleeping

--- ActualSleep/src/main/java/info/evla/actualsleep/actualsleep/SleepListener.java
+++ ActualSleep/src/main/java/info/evla/actualsleep/actualsleep/SleepListener.java
@@ -1,19 +1,30 @@
 package info.evla.actualsleep.actualsleep;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 
 public class SleepListener implements Listener {
     private ActualSleep plugin;
     private short TIME_TO_WAKE = 23458;
+    private boolean waitingForDay = false;
 
     SleepListener(ActualSleep plugin) {
         this.plugin = plugin;
     }
 
     private void setToDay(PlayerBedEnterEvent event) {
+        waitingForDay = false;
+        boolean someoneSleeps = false;
+        for (Player player : plugin.getServer().getOnlinePlayers()) {
+            if (player.isSleeping()) {
+                someoneSleeps = true;
+            }
+        }
+        if (!someoneSleeps) return;
         event.getBed().getWorld().setTime(TIME_TO_WAKE);  // Set the time to morning
         event.getBed().getWorld().setStorm(false);  // Set the weather to clear
     }
@@ -22,9 +33,12 @@ public class SleepListener implements Listener {
     public void onBedEnter(PlayerBedEnterEvent event)
     {
         if (event.getBedEnterResult().equals(PlayerBedEnterEvent.BedEnterResult.OK)) {
-            Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + " is now sleeping, the sun will soon rise!");
-            Bukkit.getScheduler().scheduleSyncDelayedTask(this.plugin, () -> setToDay(event), 100L);
-        }
 
+            Bukkit.broadcastMessage(event.getPlayer().getDisplayName() + " is now sleeping, the sun will soon rise!");
+            if (!waitingForDay) {
+                waitingForDay = true;
+                Bukkit.getScheduler().scheduleSyncDelayedTask(this.plugin, () -> setToDay(event), 100L);
+            }
+        }
     }
 }
